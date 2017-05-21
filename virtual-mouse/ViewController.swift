6c376c494c698fcb100dbd9d56bd90652da4a4bf
rutti-bep <konnosatoru@konnoakira-no-macbook.local>
2017-05-22 05:07:44 +0900

    Initial Commit

---
+++ virtual-mouse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  virtual-mouse
+//
+//  Created by 今野暁 on 2017/05/22.
+//  Copyright © 2017年 今野暁. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
